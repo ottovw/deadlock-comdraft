47060be33a58a510f0a700413a7588ead0cac868
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2024-11-16 23:13:35 +0100

    fixed types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
+import { createClient, Session } from '@supabase/supabase-js'
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLIC_API_KEY)
 
@@ -10,7 +10,7 @@ export default function App() {
 
   console.log(import.meta.env)
 
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
