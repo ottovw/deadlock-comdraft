792a85cb7dbeb26c28e6c69d709ea08d069fe492
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2024-11-16 23:12:26 +0100

    added supabase login

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
 
-function App() {
-  const [count, setCount] = useState(0)
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLIC_API_KEY)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo border-2" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-red-100">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Change for deployment test
-      </p>
-    </>
-  )
+const redirectUrl = window.location.origin + import.meta.env.BASE_URL;
+
+export default function App() {
+
+  console.log(import.meta.env)
+
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <button onClick={() => {
+        supabase.auth.signInWithOAuth({
+          provider: 'discord',
+          options: {
+            redirectTo: redirectUrl,
+          },
+        })
+      }}>Test</button>
+    )
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
 }
-
-export default App
