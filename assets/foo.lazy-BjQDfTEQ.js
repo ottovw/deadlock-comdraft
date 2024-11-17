b9083547b81f5e284f32a81161c380641e2eaca7
ottovw <ottovw@users.noreply.github.com>
2024-11-17 01:40:57 +0000

    deploy: efd8f6e88f60d11a56d256f90ee671f7c6320135

---
+++ assets/foo.lazy-BjQDfTEQ.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-SVZplkCd.js";const r=e("/foo")({component:t});function t(){return o.jsx("div",{className:"p-2",children:o.jsx("h3",{children:"FOO"})})}export{r as Route};
