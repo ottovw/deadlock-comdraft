6852dc845f17760d933c404d0591abcadb6e43be
ottovw <ottovw@users.noreply.github.com>
2024-11-17 15:45:39 +0000

    deploy: 6e590dc7bb8747a4e31868bff2b3415da0e7314d

--- assets/foo.lazy-BHEgxw_R.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o}from"./index-hNjIbDhI.js";const r=e("/foo")({component:t});function t(){return o.jsx("div",{className:"p-2",children:o.jsx("h3",{children:"FOO"})})}export{r as Route};
